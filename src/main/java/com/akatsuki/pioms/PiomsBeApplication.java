2e644677ea3d91649fe0ab2545af971f1c1baffd
악덕 <rudejrqkr97@gmail.com>
2024-05-03 16:32:55 +0900

    :tada: init

---
+++ src/main/java/com/akatsuki/pioms/PiomsBeApplication.java
@@ -0,0 +1,13 @@
+package com.akatsuki.pioms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiomsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiomsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/akatsuki/pioms/PiomsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akatsuki.pioms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiomsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
