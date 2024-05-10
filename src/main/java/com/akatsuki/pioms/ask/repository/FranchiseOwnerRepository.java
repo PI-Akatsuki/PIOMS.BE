6e355b744fe2219bdf1c376568da018645f2386b
jaehyeon-SMU <81796274+jaehyeon-SMU@users.noreply.github.com>
2024-05-10 16:05:48 +0900

    fix: ask conflic handling
    문의사항 충돌 처리

--- src/main/java/com/akatsuki/pioms/ask/repository/FranchiseOwnerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.akatsuki.pioms.ask.repository;
-
-
-import com.akatsuki.pioms.frowner.aggregate.FranchiseOwner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FranchiseOwnerRepository extends JpaRepository<FranchiseOwner, Integer> {
-}
