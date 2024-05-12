fd6b1e25a7488450609032a7ae48ae14a07a8f6a
alu98@naver.com <alu98@naver.com>
2024-05-12 13:31:37 +0900

    feat: #8 Delete CategoryThird without products by code
    상품이 속해있지 않는 카테고리(소) 카테고리 삭제 기능 구현하였습니다.

--- src/main/java/com/akatsuki/pioms/categoryThird/service/CategoryThirdServiceImpl.java
+++ src/main/java/com/akatsuki/pioms/categoryThird/service/CategoryThirdServiceImpl.java
@@ -8,6 +8,8 @@ import com.akatsuki.pioms.categoryThird.aggregate.RequestCategoryThirdPost;
 import com.akatsuki.pioms.categoryThird.aggregate.ResponseCategoryThirdPost;
 import com.akatsuki.pioms.log.etc.LogStatus;
 import com.akatsuki.pioms.log.service.LogService;
+import com.akatsuki.pioms.product.aggregate.Product;
+import com.akatsuki.pioms.product.repository.ProductRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,11 +24,13 @@ import java.util.Optional;
 public class CategoryThirdServiceImpl implements CategoryThirdService{
 
     private final CategoryThirdRepository categoryThirdRepository;
+    private final ProductRepository productRepository;
     LogService logService;
 
     @Autowired
-    public CategoryThirdServiceImpl(CategoryThirdRepository categoryThirdRepository,LogService logService) {
+    public CategoryThirdServiceImpl(CategoryThirdRepository categoryThirdRepository, ProductRepository productRepository, LogService logService) {
         this.categoryThirdRepository = categoryThirdRepository;
+        this.productRepository = productRepository;
         this.logService = logService;
     }
 
@@ -87,6 +91,24 @@ public class CategoryThirdServiceImpl implements CategoryThirdService{
         return responseValue;
     }
 
+    @Override
+    @Transactional
+    public String deleteCategoryThird(int categoryThirdCode) {
+        CategoryThird categoryThird = categoryThirdRepository.findByCategoryThirdCode(categoryThirdCode);
+        if (categoryThird == null) {
+            return categoryThirdCode + "번 카테고리(소) 카테고리가 없습니다!";
+        }
+
+        // Check if there are any products associated with this categoryThirdCode
+        List<Product> products = productRepository.findByCategoryThird_CategoryThirdCode(categoryThirdCode);
+        if (!products.isEmpty()) {
+            return "상품이 존재하는 해당 " + categoryThirdCode + "번 카테고리(소) 카테고리는 삭제할 수 없습니다!";
+        }
+
+        categoryThirdRepository.delete(categoryThird);
+        logService.saveLog("root", LogStatus.삭제,categoryThird.getCategoryThirdName(),"CategoryThird");
+        return categoryThirdCode + "번의 해당 카테고리(소) 카테고리가 성공적으로 삭제되었습니다!";
+    }
 //    @Override
 //    @Transactional
 //    public CategoryThird deleteCategory(int categoryThirdCode) {
--- src/main/java/com/akatsuki/pioms/product/repository/ProductRepository.java
+++ src/main/java/com/akatsuki/pioms/product/repository/ProductRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     List<Product> findAllByCategoryThirdCategoryThirdCode(int categoryThirdCode);
+
+    List<Product> findByCategoryThird_CategoryThirdCode(int categoryThirdCode);
 }
