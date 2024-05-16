00588bbc3e95b8f76e5fd14127855bd10d57e3cf
DESKTOP-N598GAM\동호 <alu98@naver.com>
2024-05-16 17:35:06 +0900

    test: #148 product get test
    상품 조회 관련 테스트 코드 작성했습니다.

--- src/test/java/com/akatsuki/pioms/product/service/ProductServiceTest.java
+++ src/test/java/com/akatsuki/pioms/product/service/ProductServiceTest.java
@@ -1,13 +1,35 @@
 package com.akatsuki.pioms.product.service;
 
+import com.akatsuki.pioms.product.aggregate.Product;
+import com.akatsuki.pioms.product.aggregate.RequestProduct;
+import com.akatsuki.pioms.product.repository.ProductRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class ProductServiceTest {
+    ProductRepository productRepository;
+    ProductService productService;
+
+    @Autowired
+    public ProductServiceTest(ProductRepository productRepository, ProductService productService) {
+        this.productRepository = productRepository;
+        this.productService = productService;
+    }
 
     @Test
-    void getAllProduct() {assertEquals(true,true);}
+    void getAllProduct() {
+        List<Product> product = productRepository.findAll();
+        List<Product> products = productService.getAllProduct();
+        assertEquals(product.size(), products.size());
+    }
 
     @Test
     void findProductByCode() {assertEquals(true,true);}
