2d311836cc12d59f806b4182b4496e83dafb7642
daersh <daersh@naver.com>
2024-05-16 17:43:31 +0900

    test: test get specs list, get specs list by franchise code
    - 전체 조회
    - 가맹점 코드를 통한 조회 테스트

--- src/test/java/com/akatsuki/pioms/specs/service/SpecsServiceTest.java
+++ src/test/java/com/akatsuki/pioms/specs/service/SpecsServiceTest.java
@@ -1,36 +1,69 @@
 package com.akatsuki.pioms.specs.service;
 
+import com.akatsuki.pioms.specs.aggregate.SpecsEntity;
+import com.akatsuki.pioms.specs.dto.SpecsDTO;
+import com.akatsuki.pioms.specs.repository.SpecsRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Transactional
 class SpecsServiceTest {
 
-    @Test
-    void getSpecsList() {
+    SpecsRepository specsRepository;
+    SpecsService specsService;
+    @Autowired
+    public SpecsServiceTest(SpecsRepository specsRepository, SpecsService specsService) {
+        this.specsRepository = specsRepository;
+        this.specsService = specsService;
     }
 
     @Test
-    void getSpecs() {
+    void getSpecsList() {
+        List<SpecsEntity> specsList = specsRepository.findAll();
+        List<SpecsDTO> specsDTOS = specsService.getSpecsList();
+        assertEquals(specsList.size(),specsDTOS.size());
     }
 
     @Test
     void getFranchiseSpecsList() {
+        int franchiseCode =1;
+        List<SpecsEntity> specsList = specsRepository.findAllByOrderFranchiseFranchiseCode(franchiseCode);
+        List<SpecsDTO> specsDTOS = specsService.getFranchiseSpecsList(franchiseCode);
+        assertEquals(specsList.size(),specsDTOS.size());
     }
 
     @Test
     void afterAcceptOrder() {
+
     }
 
     @Test
     void getSpecsByFranchiseCode() {
+        int franchiseCode = 1;
+        int specsCode =1;
+        SpecsEntity specs = specsRepository.findById(1).orElse(null);
+        SpecsDTO specsDTO = specsService.getSpecsByFranchiseCode(franchiseCode,1);
+        assertEquals(specs.getSpecsCode(),specsDTO.getSpecsCode());
     }
 
     @Test
     void getSpecsListByAdminCode() {
+        List<SpecsEntity> specsList = specsRepository.findAll();
+        List<SpecsDTO> specsDTOS = specsService.getSpecsList();
+        assertEquals(specsList.size(),specsDTOS.size());
     }
 
     @Test
     void getSpecsByAdminCode() {
+        List<SpecsEntity> specsList = specsRepository.findAll();
+        List<SpecsDTO> specsDTOS = specsService.getSpecsList();
+        assertEquals(specsList.size(),specsDTOS.size());
     }
 }
