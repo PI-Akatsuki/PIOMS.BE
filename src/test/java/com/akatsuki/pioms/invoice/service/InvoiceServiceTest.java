030cbc45d774bdd1b56c49610155757fca2f23c1
daersh <daersh@naver.com>
2024-05-16 11:31:50 +0900

    Test: test invoice put status
    - 송장의 배송 상태 수정 테스트

--- src/test/java/com/akatsuki/pioms/invoice/service/InvoiceServiceTest.java
+++ src/test/java/com/akatsuki/pioms/invoice/service/InvoiceServiceTest.java
@@ -1,6 +1,7 @@
 package com.akatsuki.pioms.invoice.service;
 
 import com.akatsuki.pioms.franchise.aggregate.DELIVERY_DATE;
+import com.akatsuki.pioms.invoice.aggregate.DELIVERY_STATUS;
 import com.akatsuki.pioms.invoice.aggregate.Invoice;
 import com.akatsuki.pioms.invoice.dto.InvoiceDTO;
 import com.akatsuki.pioms.invoice.repository.InvoiceRepository;
@@ -57,7 +58,7 @@ class InvoiceServiceTest {
     }
 
     @Test
-    void postInvoice() {
+    void postInvoiceAndPut() {
         // this test located in Order Test
         // 이 메서드는 주문이 생성된 이후에 바로 이루어지는 것으로 주문 후 확인 가능하다~!
         int franchiseCode = 1;
@@ -81,27 +82,25 @@ class InvoiceServiceTest {
         List<Invoice> invoicesCmp = invoiceRepository.findByOrderFranchiseFranchiseCode(franchiseCode);
         boolean result = invoiceRepository.existsByOrderOrderCode(orderDTO.getOrderCode());
         System.out.println("invoicesCmp.size() = " + invoicesCmp.size());
-//        assertNotEquals(invoicesCnt,invoicesCmp.size());
+        assertNotEquals(invoicesCnt,invoicesCmp.size());
         assertEquals(true, result);
+
+
+        // Put
+
+        //when
+        Invoice invoiceForPut = invoicesCmp.get(invoicesCmp.size()-1);
+        invoiceService.putInvoice(invoiceForPut.getInvoiceCode(), DELIVERY_STATUS.배송완료);
+        assertEquals(DELIVERY_STATUS.배송완료, invoiceForPut.getDeliveryStatus());
     }
 
-    @Test
-    void putInvoice() {
-
-    }
 
     @Test
     void getInvoice() {
 
     }
 
-    @Test
-    void checkInvoiceStatus() {
-    }
 
-    @Test
-    void saveInvoice() {
-    }
 
     @Test
     void deleteInvoice() {
@@ -111,7 +110,5 @@ class InvoiceServiceTest {
     void getInvoiceByOrderCode() {
     }
 
-    @Test
-    void afterAcceptOrder() {
-    }
+
 }
