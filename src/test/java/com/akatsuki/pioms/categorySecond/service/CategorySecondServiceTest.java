ca5811971b20c0b76262069999ca01c5638748f8
alu98@naver.com <alu98@naver.com>
2024-05-17 11:23:00 +0900

    test: #146 test code for CategorySecond
    카테고리(중) 관련 테스트 코드 작성하였습니다.

--- src/test/java/com/akatsuki/pioms/categorySecond/service/CategorySecondServiceTest.java
+++ src/test/java/com/akatsuki/pioms/categorySecond/service/CategorySecondServiceTest.java
@@ -1,5 +1,6 @@
 package com.akatsuki.pioms.categorySecond.service;
 
+import com.akatsuki.pioms.admin.aggregate.Admin;
 import com.akatsuki.pioms.categorySecond.aggregate.CategorySecond;
 import com.akatsuki.pioms.categorySecond.aggregate.RequestCategorySecondPost;
 import com.akatsuki.pioms.categorySecond.aggregate.RequestCategorySecondUpdate;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -24,8 +26,8 @@ class CategorySecondServiceTest {
     @Autowired
     private CategorySecondService categorySecondService;
 
-    static RequestCategorySecondPost postRequest;
-    static RequestCategorySecondUpdate updateRequest;
+    static RequestCategorySecondPost requestPost;
+    static RequestCategorySecondUpdate requestUpdate;
 
     @Test
     @DisplayName("카테고리(중) 전체 조회")
@@ -45,7 +47,19 @@ class CategorySecondServiceTest {
     }
 
     @Test
-    void postCategorySecond() {assertEquals(true,true);}
+    void postCategorySecond() {
+        Admin requestorAdmin = new Admin();
+        requestorAdmin.setAdminCode(1);
+
+        CategorySecond categorySecond = new CategorySecond();
+        categorySecond.setCategorySecondName("postTest");
+        categorySecond.setCategorySecondEnrollDate("2024-05-17 00:00:00");
+        categorySecond.setCategorySecondUpdateDate("2024-05-17 00:00:00");
+
+        ResponseEntity<String> response = categorySecondService.postCategorySecond(requestPost, categorySecond.getCategorySecondCode());
+
+        assertEquals("신규 카테고리 등록", response.getBody());
+    }
 
     @Test
     void updateCategorySecond() {assertEquals(true,true);}
