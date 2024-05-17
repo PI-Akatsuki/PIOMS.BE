3f87018eb279a44bc0ae44026f3d08997530a32e
daersh <daersh@naver.com>
2024-05-17 15:35:32 +0900

    docs: Add Docs in Invoice serivce
    - 배송 서비스에 메서드 설명 문서 추가

--- src/main/java/com/akatsuki/pioms/invoice/service/InvoiceService.java
+++ src/main/java/com/akatsuki/pioms/invoice/service/InvoiceService.java
@@ -11,26 +11,40 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface InvoiceService {
-    List<InvoiceDTO> getAllInvoiceList();
-    InvoiceDTO postInvoice(OrderDTO order);
 
+    /**
+     * .@Copyright 32173594@dankook.ac.kr
+     * <br><br><br>
+     * <h1>Invoice Service</h1>
+     *     <br>
+     * <h1>Admin Functions</h1>
+     * <h2>1. Get Admin Invoice List : 관리자가 관리하고 있는 모든 가맹점의 송장 목록 반환</h2>
+     * <h2>2. Get Invoice By Admin Code : 관리자가 관리하고 있는 모든 가맹점의 송장 목록 중 하나 상세 조회 </h2>
+     * <h2>3.  putInvoice : 관리자의 배송 상태 수정 </h2>
+     * <br>
+     *
+     * <h1> Franchise Owner Functions</h1>
+     * <h2>1.Get Franchise Invoice List : 점주의 모든 송장 목록 반환 </h2>
+     * <h2>2.Get Invoice By Franchise Owner Code : 점주의 송장 상세 조회 </h2>
+     * <br>
+     *
+     * <h1> INNER FUNCTIONS </h1>
+     * <h2>1. After Accept Order : 관리자가 주문을 승인한 이후 새로운 송장을 생성하는 로직 </h2>
+     * <h2>2. Check Invoice Status : 주문 배송 상태 조회하기 위한 로직 </h2>
+     * <h2>3. Get Invoice By Order Code : 주문 코드를 통해 송장 조회하는 로직</h2>
+     * <h2>4. Delete Invoice : 아직 처리 되지 않은 주문 삭제 </h2>
+     * */
+
+    List<InvoiceDTO> getAdminInvoiceList(int adminCode);
+    InvoiceDTO getInvoiceByAdminCode(int adminCode, int invoiceCode);
     InvoiceDTO putInvoice(int adminCode,int invoiceCode, DELIVERY_STATUS invoiceStatus);
 
-    Boolean checkInvoiceStatus(int orderCode);
-
-    InvoiceDTO saveInvoice(Invoice invoiceEntity);
-
-    boolean deleteInvoice(int franchiseOwnerCode, int invoiceCode);
-
-    InvoiceDTO getInvoiceByOrderCode(int orderCode);
+    List<InvoiceDTO> getFranchiseInvoiceList(int franchiseOwnerCode);
+    InvoiceDTO getInvoiceByFranchiseOwnerCode(int franchiseOwnerCode, int invoiceCode);
 
     void afterAcceptOrder(OrderDTO orderEntity);
 
-    List<InvoiceDTO> getAdminInvoiceList(int adminCode);
-
-    InvoiceDTO getInvoiceByAdminCode(int adminCode, int invoiceCode);
-
-    List<InvoiceDTO> getFranchiseInvoiceList(int franchiseOwnerCode);
-
-    InvoiceDTO getInvoiceByFranchiseOwnerCode(int franchiseOwnerCode, int invoiceCode);
+    Boolean checkInvoiceStatus(int orderCode);
+    InvoiceDTO getInvoiceByOrderCode(int orderCode);
+    boolean deleteInvoice(int franchiseOwnerCode, int invoiceCode);
 }
