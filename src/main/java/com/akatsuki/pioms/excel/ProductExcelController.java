ad1fab4ea869c83250f9e1453da7ab2f18c5b97f
alu98@naver.com <alu98@naver.com>
2024-05-18 17:40:22 +0900

    refactor: #186 Excel download feature
    엑셀 다운로드 기능 코드 리팩토링했습니다.

--- src/main/java/com/akatsuki/pioms/excel/ProductExcelController.java
+++ src/main/java/com/akatsuki/pioms/excel/ProductExcelController.java
@@ -7,7 +7,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
