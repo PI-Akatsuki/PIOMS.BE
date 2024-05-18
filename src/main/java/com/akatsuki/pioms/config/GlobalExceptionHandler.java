3d1f0746fff92b5f0b1ea3e82a5d6b4af3e7c461
DESKTOP-N598GAM\동호 <alu98@naver.com>
2024-05-19 00:03:52 +0900

    refactor: #186 Excel download file annotation for test
    엑셀 관련 코드 주석 처리하였습니다.

--- src/main/java/com/akatsuki/pioms/config/GlobalExceptionHandler.java
+++ src/main/java/com/akatsuki/pioms/config/GlobalExceptionHandler.java
@@ -1,26 +1,3 @@
-//package com.akatsuki.pioms.config;
-//
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-//
-//    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<String> handleAllException(Exception ex) {
-//        logger.error("Error occurred: ", ex);
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An internal server error occurred");
-//    }
-//}
-//
 package com.akatsuki.pioms.config;
 
 import org.springframework.http.HttpStatus;
