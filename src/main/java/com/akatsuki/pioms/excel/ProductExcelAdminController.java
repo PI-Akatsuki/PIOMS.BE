2430f7d9e782430c9a56a6afcd97efddc0405c5e
alu98@naver.com <alu98@naver.com>
2024-05-20 14:08:05 +0900

    feat: #204 excel download Admin-Product
    관리자가 상품 목록을 엑셀로 다운할 수 있는 기능 구현 완료하였습니다.

--- src/main/java/com/akatsuki/pioms/excel/ProductExcelAdminController.java
+++ src/main/java/com/akatsuki/pioms/excel/ProductExcelAdminController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/exceldownload")
+@RequestMapping("admin/exceldownload")
 @ComponentScan
 public class ProductExcelAdminController {
 
