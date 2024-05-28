d0750b27cc14327f86f280868f943c97fadfedea
jaehyeon-SMU <81796274+jaehyeon-SMU@users.noreply.github.com>
2024-05-28 10:30:04 +0900

    Refactor: refactoring Log
    로그 전체 조회를 위한 기능 구현 (코드 리펙토링)

--- src/main/java/com/akatsuki/pioms/log/repository/LogRepository.java
+++ src/main/java/com/akatsuki/pioms/log/repository/LogRepository.java
@@ -2,6 +2,8 @@ package com.akatsuki.pioms.log.repository;
 
 import com.akatsuki.pioms.log.aggregate.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
 }
