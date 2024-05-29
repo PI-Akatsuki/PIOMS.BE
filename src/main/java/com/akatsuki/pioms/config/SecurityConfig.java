fe3ff5f20bad2e25f58cfe26035c54618b08a9b7
악덕 <rudejrqkr97@gmail.com>
2024-05-29 12:31:50 +0900

    fix: Fix CORS configuration in WebConfig and SecurityConfig
    - WebConfig.java 파일에서 CORS 구성 수정
    - WebConfig.java 파일의 CORS 설정 수정
    - SecurityConfig.java 파일의 CORS 설정 추가
    - SecurityConfig.java 파일에서 CORS 설정 추가

--- src/main/java/com/akatsuki/pioms/config/SecurityConfig.java
+++ src/main/java/com/akatsuki/pioms/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.akatsuki.pioms.config;
 import com.akatsuki.pioms.jwt.JWTFilter;
 import com.akatsuki.pioms.jwt.JWTUtil;
 import com.akatsuki.pioms.jwt.LoginFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,19 +13,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -34,13 +25,14 @@ public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
+    private final CorsConfigurationSource corsConfigurationSource;
 
     @Autowired
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil, CorsConfigurationSource corsConfigurationSource) {
         this.authenticationConfiguration = authenticationConfiguration;
         this.jwtUtil = jwtUtil;
+        this.corsConfigurationSource = corsConfigurationSource;
     }
-
 
     // AuthenticationManager Bean 등록
     @Bean
@@ -50,41 +42,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
                 .csrf(csrf -> csrf.disable())
                 .formLogin(formLogin -> formLogin.disable())
-                .httpBasic(httpBasic -> httpBasic.disable());
-
-
-        http
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource)) // CORS 설정 추가
                 .authorizeHttpRequests(authorize -> authorize
-                                .anyRequest().permitAll()
-//                        .requestMatchers("/reissue", "/login", "/admin/login", "/franchise/login", "/driver/login").permitAll()
-//                        .requestMatchers("/admin/**").hasRole("ROOT")
-//                        .requestMatchers(
-//                                "/admin/info",
-//                                "/admin/list/**",
-//                                "/admin/category/first/list/**",
-//                                "/admin/category/second/list/**",
-//                                "/admin/category/third/list/**",
-//                                "/admin/driver/list/**",
-//                                "/admin/franchise/list/**",
-//                                "/admin/franchise/owner/list/**",
-//                                "/admin/franchise/owner/update/**",
-//                                "/admin/product/list/**",
-//                                "/admin/specs/**",
-//                                "/admin/order/**",
-//                                "/admin/invoice/**",
-//                                "/admin/exchange/**",
-//                                "/admin/notice/list/**",
-//                                "/admin/ask/**",
-//                                "/admin/exceldownload/**").hasRole("ADMIN")
-//                        .requestMatchers("/franchise/**").hasRole("OWNER")
-//                        .requestMatchers("/driver/**").hasRole("DRIVER")
-//                        .anyRequest().authenticated()
-                );
-        http
+                        .requestMatchers("/reissue", "/login", "/admin/login", "/franchise/login", "/driver/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ROOT")
+                        .requestMatchers(
+                                "/admin/info",
+                                "/admin/list/**",
+                                "/admin/category/first/list/**",
+                                "/admin/category/second/list/**",
+                                "/admin/category/third/list/**",
+                                "/admin/driver/list/**",
+                                "/admin/franchise/list/**",
+                                "/admin/franchise/owner/list/**",
+                                "/admin/franchise/owner/update/**",
+                                "/admin/product/list/**",
+                                "/admin/specs/**",
+                                "/admin/order/**",
+                                "/admin/invoice/**",
+                                "/admin/exchange/**",
+                                "/admin/notice/list/**",
+                                "/admin/ask/**",
+                                "/admin/exceldownload/**").hasRole("ADMIN")
+                        .requestMatchers("/franchise/**").hasRole("OWNER")
+                        .requestMatchers("/driver/**").hasRole("DRIVER")
+                        .anyRequest().authenticated()
+                )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login?logout=true")
@@ -97,36 +84,11 @@ public class SecurityConfig {
 
         // Session 설정
         http
-                .sessionManagement((session) -> session
+                .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-//                .sessionManagement(session -> session
-//                        .maximumSessions(5)
-//                        .maxSessionsPreventsLogin(true)
-//                )
-//                .sessionManagement(session -> session
-//                        .sessionFixation().changeSessionId()
-//                );
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
         return http.build();
     }
-
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOriginPatterns(Collections.singletonList("http://localhost:8080"));
-//        configuration.setAllowedOriginPatterns(Collections.singletonList("http://api.pioms.shop"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//        configuration.setAllowCredentials(true);
-//        configuration.setAllowedHeaders(Collections.singletonList("*"));
-//        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie"));
-//        configuration.setMaxAge(3600L);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", configuration);
-//        return source;
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -140,5 +102,4 @@ public class SecurityConfig {
         roleHierarchy.setHierarchy(hierarchy);
         return roleHierarchy;
     }
-
 }
