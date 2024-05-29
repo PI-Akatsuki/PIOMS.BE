e6a7bb7492a65c200e8a72b7f9b5651cde39c91e
악덕 <rudejrqkr97@gmail.com>
2024-05-29 10:10:40 +0900

    hotfix: correct allowed origin URL in WebConfig
    - allowed origin URL in WebConfig has been updated
    - to 'http://api.pioms.shop' for Vue deployment server.

--- src/main/java/com/akatsuki/pioms/config/WebConfig.java
+++ src/main/java/com/akatsuki/pioms/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                         "http://localhost:5173",   // Vue 개발 서버
                         "http://localhost:3000",   // Vue 배포 서버
-                        "http://spioms.shop"   // Vue 배포 서버
+                        "http://api.pioms.shop"   // Vue 배포 서버
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
