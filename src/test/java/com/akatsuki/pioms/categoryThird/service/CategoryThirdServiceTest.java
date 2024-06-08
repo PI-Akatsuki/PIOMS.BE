8955d63c81cfba0868006ddc3c72184a422a8694
letsplaycoding <sdh989800@gmail.com>
2024-06-08 21:31:29 +0900

    feat: #147 test for CategoryThird
    카테고리 소분류 테스트 코드 작성

--- src/test/java/com/akatsuki/pioms/categoryThird/service/CategoryThirdServiceTest.java
+++ src/test/java/com/akatsuki/pioms/categoryThird/service/CategoryThirdServiceTest.java
@@ -3,14 +3,19 @@ package com.akatsuki.pioms.categoryThird.service;
 import com.akatsuki.pioms.categoryThird.aggregate.CategoryThird;
 import com.akatsuki.pioms.categoryThird.aggregate.RequestCategoryThird;
 import com.akatsuki.pioms.categoryThird.aggregate.ResponseCategoryThird;
+import com.akatsuki.pioms.categoryThird.dto.CategoryThirdCreateDTO;
 import com.akatsuki.pioms.categoryThird.dto.CategoryThirdDTO;
 import com.akatsuki.pioms.categoryThird.repository.CategoryThirdRepository;
+import com.akatsuki.pioms.log.etc.LogStatus;
+import com.akatsuki.pioms.log.service.LogServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,15 +31,19 @@ class CategoryThirdServiceTest {
     @Autowired
     private CategoryThirdService categoryThirdService;
 
-    @Autowired
+    @MockBean
     private CategoryThirdRepository categoryThirdRepository;
 
     @MockBean
-    private MockMvc mvc;
+    private LogServiceImpl logService;
 
-    static RequestCategoryThird request;
-    private RequestBuilder delete;
+    private CategoryThird categoryThird;
 
+    @BeforeEach
+    void init() {
+        categoryThird = new CategoryThird();
+        categoryThirdRepository.save(categoryThird);
+    }
 
     @Test
     @DisplayName("카테고리(소) 전체 조회")
@@ -65,51 +74,72 @@ class CategoryThirdServiceTest {
 
     @Test
     @DisplayName("카테고리(소) 신규 등록")
-    void postCategoryThird() {
-        RequestCategoryThird requestCategory = new RequestCategoryThird();
-        requestCategory.setCategoryThirdCode(20);
-        requestCategory.setCategoryThirdName("test");
-        requestCategory.setCategorySecondCode(1);
+    @WithMockUser(username = "root", roles = {"ROOT"})
+    void createCategoryThird() {
+        // Given
+        CategoryThirdCreateDTO categoryThirdCreateDTO = new CategoryThirdCreateDTO();
+        categoryThirdCreateDTO.setCategoryThirdName("Test Name");
 
-        ResponseEntity<String> postCategory = categoryThirdService.postCategory(requestCategory, 1);
-        System.out.println("postCategory = " + postCategory);
+        // When
+        CategoryThirdDTO result = categoryThirdService.createCategoryThird(categoryThirdCreateDTO);
+        logService.saveLog("root", LogStatus.등록, categoryThird.getCategoryThirdName(), "CategoryThird");
 
-        assertNotNull(postCategory);
-        assertEquals("카테고리(소) 생성 완료!", postCategory.getBody());
+        assertNotNull(result);
+        assertEquals("Test Name", result.getCategoryThirdName());
     }
 
     @Test
     @DisplayName("카테고리(소) 수정")
-    void updateCategoryThird() {
-        RequestCategoryThird requestCategory = new RequestCategoryThird();
-        requestCategory.setCategorySecondCode(1);
-        requestCategory.setCategoryThirdName("test00");
+    @WithMockUser(username = "root", roles = {"ROOT"})
 
-        ResponseEntity<String> updateCategory = categoryThirdService.updateCategory(1, requestCategory, 1);
-        System.out.println("updateCategory = " + updateCategory);
 
-        assertNotNull(updateCategory);
-        assertEquals("카테고리(소) 수정 완료!", updateCategory.getBody());
-    }
-
-    @Test
-    void deleteCategory() {
-        // Given: 새로운 카테고리를 생성합니다.
-        RequestCategoryThird requestCategory = new RequestCategoryThird();
-        requestCategory.setCategoryThirdCode(20);
-        requestCategory.setCategoryThirdName("test");
-        requestCategory.setCategorySecondCode(1);
-
-        ResponseEntity<String> postCategory = categoryThirdService.postCategory(requestCategory, 1);
-
-        // When: 해당 카테고리를 삭제합니다.
-        categoryThirdService.deleteCategoryThird(20, 1);
-
-        // Then: 삭제된 카테고리가 데이터베이스에 없는지 확인합니다.
-        List<ResponseCategoryThird> categoryThirdList = categoryThirdService.getCategoryThirdInSecond(requestCategory.getCategorySecondCode());
-        boolean categoryExists = categoryThirdList.stream()
-                .anyMatch(category -> category.getCategoryThirdCode() == requestCategory.getCategoryThirdCode());
-        assertFalse(categoryExists, "삭 성");
-
-    }
 }
+//    @Test
+//    @DisplayName("카테고리(소) 신규 등록")
+//    void postCategoryThird() {
+//        RequestCategoryThird requestCategory = new RequestCategoryThird();
+//        requestCategory.setCategoryThirdCode(20);
+//        requestCategory.setCategoryThirdName("test");
+//        requestCategory.setCategorySecondCode(1);
+//
+//        ResponseEntity<String> postCategory = categoryThirdService.postCategory(requestCategory);
+//        System.out.println("postCategory = " + postCategory);
+//
+//        assertNotNull(postCategory);
+//        assertEquals("카테고리(소) 생성 완료!", postCategory.getBody());
+//    }
+//
+//    @Test
+//    @DisplayName("카테고리(소) 수정")
+//    void updateCategoryThird() {
+//        RequestCategoryThird requestCategory = new RequestCategoryThird();
+//        requestCategory.setCategorySecondCode(1);
+//        requestCategory.setCategoryThirdName("test00");
+//
+//        ResponseEntity<String> updateCategory = categoryThirdService.updateCategory(1, requestCategory);
+//        System.out.println("updateCategory = " + updateCategory);
+//
+//        assertNotNull(updateCategory);
+//        assertEquals("카테고리(소) 수정 완료!", updateCategory.getBody());
+//    }
+//
+//    @Test
+//    void deleteCategory() {
+//        // Given: 새로운 카테고리를 생성합니다.
+//        RequestCategoryThird requestCategory = new RequestCategoryThird();
+//        requestCategory.setCategoryThirdCode(20);
+//        requestCategory.setCategoryThirdName("test");
+//        requestCategory.setCategorySecondCode(1);
+//
+//        ResponseEntity<String> postCategory = categoryThirdService.postCategory(requestCategory);
+//
+//        // When: 해당 카테고리를 삭제합니다.
+//        categoryThirdService.deleteCategoryThird(20);
+//
+//        // Then: 삭제된 카테고리가 데이터베이스에 없는지 확인합니다.
+//        List<ResponseCategoryThird> categoryThirdList = categoryThirdService.getCategoryThirdInSecond(requestCategory.getCategorySecondCode());
+//        boolean categoryExists = categoryThirdList.stream()
+//                .anyMatch(category -> category.getCategoryThirdCode() == requestCategory.getCategoryThirdCode());
+//        assertFalse(categoryExists, "삭 성");
+//
+//    }
